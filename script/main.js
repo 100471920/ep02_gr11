6c05b6b0239f34f52b7f0a79dcff78178cf4e342
100472051 <100472051@alumnos.uc3m.es>
2023-10-18 11:02:23 +0200

    sections

---
+++ script/main.js
@@ -0,0 +1,54 @@
+/*
+    Authors:
+        Santiago Kiril Cenkov Stoyanov / 100472051@alumnos.uc3m.es
+        Oscar Hontoria Herrador / 100471920@alumnos.uc3m.es
+*/
+
+
+let timeout_ids = {"slide_drinks": 0};
+
+
+//showSlide("slide_text_drinks");
+showSlide("slide_drinks");
+//showSlide("slide_text_meals");
+//showSlide("slide_meals");
+
+
+function showSlide(slide_class, slide_index=0)
+{
+    let slides = document.getElementsByClassName(slide_class);
+    for (let i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display = "none";
+    }
+
+    slides[slide_index].style.display = "block";
+    slide_index = (slide_index + 1) % slides.length
+    timeout_ids[slide_class] = setTimeout(showSlide, 1000, slide_class, slide_index);
+}
+
+
+function stopSliding(slide_class)
+{
+    clearTimeout(timeout_ids[slide_class]);
+}
+
+
+function resumeSliding(slide_class, slide_index)
+{
+    showSlide(slide_class, slide_index);
+}
+
+
+function gotoSlide(slide_class, slide_index)
+{
+    showSlide(slide_class, slide_index);
+}
+
+
+/*
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) =>
+{
+console.log("sent");
+});*/
