3443e5374185c6e1ca4667b9345cd8cd9694bf0a
100471920 <100471920@alumnos.uc3m.es>
2023-10-19 21:39:33 +0200

    primer intento

---
+++ script/menu.js
@@ -0,0 +1,8 @@
+/*
+    Authors:
+        Santiago Kiril Cenkov Stoyanov / 100472051@alumnos.uc3m.es
+        Oscar Hontoria Herrador / 100471920@alumnos.uc3m.es
+*/
+function add_product() {
+
+      };
